d59a4f4b37e9146038de062cebc110d81f1337f8
Silzinc <jonasbloch854@proton.me>
2025-08-20 17:47:59 +0200

    fix: add boolean placeholder arg for unsupported wallpaper::get

--- material-you-derive-palette/wallpaper/unsupported.rs
+++ material-you-derive-palette/wallpaper/unsupported.rs
@@ -2,7 +2,7 @@ use std::path::PathBuf;
 
 use anyhow::{Result, anyhow};
 
-pub(crate) fn get() -> Result<PathBuf> {
+pub(crate) fn get(_dark_mode: bool) -> Result<PathBuf> {
   Err(anyhow!(
     "Wallpaper detection: your operating system is not supported."
   ))
